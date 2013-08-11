986380407b4d62861d2fd68f5bc9d485e65d2d02
Oriol Torrent Florensa <oriol.tf@me.com>
2013-08-12 00:08:46 +0200

    Setup middleman-gh-pages gem

--- config.rb
+++ config.rb
@@ -162,7 +162,7 @@ end
 # To deploy to a remote branch via git (e.g. master on github):
 ###
 
-require 'middleman-gh-pages'
+# require 'middleman-gh-pages'
 
 # activate :deploy do |deploy|
 # 	deploy.method = :git
