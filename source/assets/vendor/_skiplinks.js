081872ae2020c6d409fbb200674e669bbe07be50
Oriol Torrent Florensa <oriol.tf@me.com>
2013-10-03 17:48:57 +0200

    Indentation updates to tabs

--- source/assets/vendor/_skiplinks.js
+++ source/assets/vendor/_skiplinks.js
@@ -5,15 +5,15 @@
  */
 
 (function(a,b,c,d) {
-    for (c in a) {
-        if ((d=a[c].hash)&&a[c].href==b+d&&a[c].addEventListener) {
-            a[c].addEventListener('click',function(e,f,g,h) {
-                if (e=(f=document).getElementById(g=this.hash.slice(1))||f.getElementsByName(g)[0]) {
-                    if (h=!e.getAttribute(f='tabindex')) e.setAttribute(f,-1);
-                    e.focus();
-                    if (h) e.removeAttribute(f);
-                }
-            });
-        }
-    }
+	for (c in a) {
+		if ((d=a[c].hash)&&a[c].href==b+d&&a[c].addEventListener) {
+			a[c].addEventListener('click',function(e,f,g,h) {
+				if (e=(f=document).getElementById(g=this.hash.slice(1))||f.getElementsByName(g)[0]) {
+					if (h=!e.getAttribute(f='tabindex')) e.setAttribute(f,-1);
+					e.focus();
+					if (h) e.removeAttribute(f);
+				}
+			});
+		}
+	}
 })(document.links,location.href.split('#')[0]);
