c18d45bc04b8794b9317ef16ec7ad702dcd65090
Oriol Torrent Florensa <oriol.tf@me.com>
2016-02-03 12:40:32 +0100

    Initial Commit: Using Estático

---
+++ source/assets/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * @requires ../../../node_modules/jquery/dist/jquery.js
+ * @requires ../.tmp/lodash.js
+ * @requires ../../../node_modules/handlebars/dist/handlebars.js
+ * @requires ../.tmp/templates.js
+
+ * @requires helpers/module.js
+ * @requires helpers/events.js
+ * @requires helpers/mediaqueries.js
+ *
+ * @requires ../../demo/modules/skiplinks/skiplinks.js
+ * @requires ../../demo/modules/slideshow/slideshow.js
+ * //*autoinsertmodule*
+ *
+ * @requires helpers/init.js
+ */
